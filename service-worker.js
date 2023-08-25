cb3ff84ab7a7c8b8057ec9f73bf0a02144a18379
gisonline <oskooei@gmail.com>
2023-08-25 22:39:44 +0330

    installable

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ workbox.setConfig({
 });
 
 workbox.precaching.precacheAndRoute([
-    {url: 'manifest.json', revision: '1.3'},
+    {url: 'manifest.json', revision: '1'},
     {url: 'index.html', revision: '1'},
     {url: 'assets/css/ol.css', revision: '3'},
     {url: 'assets/css/jquery.mobile-1.4.5.css', revision: '1.4'},
@@ -19,10 +19,8 @@ workbox.precaching.precacheAndRoute([
     {url: 'assets/img/sat.gif', revision: '1'},
     {url: 'assets/img/wifioff.png', revision: '1'},
     {url: 'assets/img/network.png', revision: '1'},
-    {url: 'assets/_assets/img/bg-pattern.gif', revision: '1'},
-    {url: 'favicon.ico', revision: '1'},
     {url: 'assets/img/fire.gif', revision: '1'},
-    {url: 'assets/js/jquery-2.2.4.min.js', revision: '2'},
+    {url: 'assets/js/jquery.min.js', revision: '2'},
     {url: 'assets/js/jquery.mobile-1.4.5.min.js', revision: '1'},
     {url: 'assets/js/proj4.js', revision: '1'},
     {url: 'assets/js/ol3/ol.js', revision: '1'},
@@ -32,7 +30,7 @@ workbox.precaching.precacheAndRoute([
     {url: 'assets/js/config.js', revision: '1'},
     {url: 'assets/js/functions.js', revision: '1'},
     {url: 'assets/js/start.js', revision: '1'},
-    {url: 'assets/js/awake.js', revision: '1'},
+//    {url: 'assets/js/awake.js', revision: '1'},
 ], {
     // Ignore all URL parameters.
     ignoreURLParametersMatching: [/.*/]
