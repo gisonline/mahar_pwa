8ce96814fd0c0f6513201f04adaf302893d9c446
gisonline <oskooei@gmail.com>
2023-08-26 18:25:42 +0330

    new

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,7 @@ workbox.precaching.precacheAndRoute([
     {url: 'assets/css/wapco-theme.css', revision: '1'},
     {url: 'assets/js/jquery.min.js', revision: '2'},
     {url: 'assets/js/jquery.mobile-1.4.5.min.js', revision: '1'},
-    {url: 'assets/js/jquery-3.7.0.min.js', revision: '2'},
+//    {url: 'assets/js/jquery-3.7.0.min.js', revision: '2'},
     {url: 'assets/img/apple-touch-icon.png', revision: '1'},
     {url: 'assets/img/favicon-32x32.png', revision: '1'},
     {url: 'assets/img/favicon-16x16.png', revision: '1'},
